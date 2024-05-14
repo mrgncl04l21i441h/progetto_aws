17b180e04c53e9291e26c77159233705a171ff79
mrgncl04l21i441h <mrgncl04l21i441h@isisbernocchi.edu.it>
2024-05-15 00:28:36 +0200

    modifica login

--- login.php
+++ login.php
@@ -5,12 +5,12 @@ $password = $_POST['password'];
 if (isset($username) && isset($password)) {
     require "connect.php";
         
-    $query = "SELECT * FROM utenti WHERE username = '".$username."' AND password = '" . md5($password) . "';";
+    $query = "SELECT * FROM utenti WHERE username = '".$username."' AND password = '" . $password . "';";
     $result = $conn->query($query);
     if ($result->num_rows > 0) {
         header("location: relazione.html");
     } else {
-        echo "errore con le credenziali <a href='login.php'>login</a>";
+        echo "errore con le credenziali <a href='index.html'>login</a>";
     }
 }else{
     echo "errore bello brutto con i dati ;(";
