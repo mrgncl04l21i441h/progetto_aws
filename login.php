a981cad3e522ee730c7e38f9696f02a63463b9f8
mrgncl04l21i441h <mrgncl04l21i441h@isisbernocchi.edu.it>
2024-05-15 00:17:55 +0200

    correzzione login

--- login.php
+++ login.php
@@ -4,13 +4,15 @@ $password = $_POST['password'];
 
 if (isset($username) && isset($password)) {
     require "connect.php";
-    
-    $query = "SELECT * FROM utenti where username='".$username."' AND password='" . md5($password) . "';";
+        
+    $query = "SELECT * FROM utenti WHERE username='".$username."' AND password='" . md5($password) . "';";
     $result = $conn->query($query);
-    if ($result->num_rows == 0) {
+    if ($result->num_rows > 0) {
         header("location: relazione.html");
     } else {
         echo "errore con le credenziali <a href='login.php'>login</a>";
     }
+}else{
+    echo "errore bello brutto con i dati ;(";
 }
 ?>
