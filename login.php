183854460f827fa4a0405855ecdbdd6452bcb126
mrgncl04l21i441h <mrgncl04l21i441h@isisbernocchi.edu.it>
2024-05-15 00:42:03 +0200

    diobestia correzione login

--- login.php
+++ login.php
@@ -4,13 +4,18 @@ $password = $_POST['password'];
 
 if (isset($username) && isset($password)) {
     require "connect.php";
-        
-    $query = "SELECT * FROM utenti WHERE username = '".$username."' AND password = '" . $password . "';";
-    $result = mysqli_query($conn, $query);
+    
+    
+
+    $query = "SELECT * FROM utenti WHERE username = '".$username."' AND password = '".$password."';";
+    $result = $conn->query($query);
     if ($result->num_rows > 0) {
         header("location: relazione.html");
     } else {
         echo "errore con le credenziali <a href='index.html'>login</a>";
+        echo $username;
+        echo $password;
+        echo $conn->error;
     }
 }else{
     echo "errore bello brutto con i dati ;(";
